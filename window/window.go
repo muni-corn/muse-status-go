bc9ea242adc8c599a0b55f5d62de228050614a24
municorn <municorn@musicaloft.com>
2019-04-19 21:18:40 -0600

    the window module is pretty much dead now
    so i'm commenting out all of its code instead of rewriting it for the
    new blocks

--- window/window.go
+++ window/window.go
@@ -1,57 +1,57 @@
 package window
 
-import (
-	"github.com/muni-corn/muse-status/date"
-	"github.com/muni-corn/muse-status/format"
+// import (
+// 	"github.com/muni-corn/muse-status/date"
+// 	"github.com/muni-corn/muse-status/format"
 
-	"os/exec"
-	"regexp"
-	"strings"
-	"time"
-	// "encoding/json"
-)
+// 	"os/exec"
+// 	"regexp"
+// 	"strings"
+// 	"time"
+// 	// "encoding/json"
+// )
 
-var (
-	lineReturnRegex = regexp.MustCompile(`\r?\n`)
-)
+// var (
+// 	lineReturnRegex = regexp.MustCompile(`\r?\n`)
+// )
 
-// StartWindowBroadcast returns a string channel that is fed info about the
-// current active window. If no window is active, it is fed a greeting or
-// information useful to the user.
-func StartWindowBroadcast() chan *format.ClassicBlock {
-	channel := make(chan *format.ClassicBlock)
-	block := &format.ClassicBlock{Name: "window"}
+// // StartWindowBroadcast returns a string channel that is fed info about the
+// // current active window. If no window is active, it is fed a greeting or
+// // information useful to the user.
+// func StartWindowBroadcast() chan *format.ClassicBlock {
+// 	channel := make(chan *format.ClassicBlock)
+// 	block := &format.ClassicBlock{Name: "window"}
 
-	go func() {
-		var lastWindow string
-		for {
-			currentWindow := window()
-			if lastWindow != currentWindow {
-				block.PrimaryText = currentWindow
-				channel <- block
-				lastWindow = currentWindow
-			}
+// 	go func() {
+// 		var lastWindow string
+// 		for {
+// 			currentWindow := window()
+// 			if lastWindow != currentWindow {
+// 				block.PrimaryText = currentWindow
+// 				channel <- block
+// 				lastWindow = currentWindow
+// 			}
 
-			time.Sleep(time.Second / 10)
-		}
-	}()
+// 			time.Sleep(time.Second / 10)
+// 		}
+// 	}()
 
-	return channel
-}
+// 	return channel
+// }
 
-func window() string {
-	// get sway tree
-	cmdOutput, err := exec.Command("swaymsg", "-t", "get_tree").Output()
-	if err != nil {
-		return date.GetGreeting()
-	}
+// func window() string {
+// 	// get sway tree
+// 	cmdOutput, err := exec.Command("swaymsg", "-t", "get_tree").Output()
+// 	if err != nil {
+// 		return date.GetGreeting()
+// 	}
 
-	output := string(cmdOutput)
-	if output == "i3" || strings.TrimSpace(output) == "" {
-		output = date.GetGreeting()
-	} else {
-		output = lineReturnRegex.ReplaceAllString(output, "")
-	}
+// 	output := string(cmdOutput)
+// 	if output == "i3" || strings.TrimSpace(output) == "" {
+// 		output = date.GetGreeting()
+// 	} else {
+// 		output = lineReturnRegex.ReplaceAllString(output, "")
+// 	}
 
-	return format.Dim(output)
-}
+// 	return format.Dim(output)
+// }
