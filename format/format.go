08e801980a7d6a4727fa914f533d9f058ee9ce66
municorn <municorn@musicaloft.com>
2019-07-06 01:43:15 -0600

    added lemonbar mode back in, deleted now obsolete functions

--- format/format.go
+++ format/format.go
@@ -2,12 +2,23 @@ package format
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
-	"time"
+	"strconv"
 )
 
-var textFont, iconFont = "Noto Sans 10", "Material Design Icons 12"
+var textFont, iconFont = "Roboto 10", "Material Design Icons 12"
+
+// Mode is for different types of status modes, for different status bars that
+// parse information differently
+type Mode int
+
+var mode Mode
+
+// Definitions for Mode
+const (
+	I3JSONMode Mode = iota
+	LemonbarMode
+)
 
 // Chain chains status bites together, ensuring that there are no
 // awkward spaces between bites.
@@ -25,14 +36,16 @@ func Chain(blocks ...DataBlock) string {
 		return ""
 	}
 
-	final = JSONOf(blocks[first])
+	final = I3JSONOf(blocks[first])
 
 	for i := first + 1; i < len(blocks); i++ {
 		if blocks[i] == nil || blocks[i].Hidden() {
 			continue
 		}
-		v := JSONOf(blocks[i])
-		v = strings.ReplaceAll(v, "&", `&amp;`) // escape ampersand for json
+		v := I3JSONOf(blocks[i])
+		if mode == I3JSONMode {
+			v = Escape(v);
+		}
 
 		// trim space at the ends
 		v = strings.TrimSpace(v)
@@ -44,133 +57,19 @@ func Chain(blocks ...DataBlock) string {
 	return final
 }
 
+// Escape escapes characters for the i3 json protocol
+func Escape(original string) string {
+	return strings.ReplaceAll(original, "&", `&amp;`) // escape ampersand for json
+}
+
 // Action returns the original text wrapped in a clickable
 // area
 func Action(action, original string) string {
-	if mode == ModeI3Bar {
+	if mode == I3JSONMode {
 		return original
 	}
 	s := fmt.Sprintf("%%{A:%s:}%s%%{A}", action, original)
 	return s
-}
-
-// Left aligns the original string to the left
-func Left(original string) string {
-	if mode == ModeI3Bar {
-		return original
-	}
-	return "%{l}" + original
-}
-
-// Center aligns the original string to the center
-func Center(original string) string {
-	if mode == ModeI3Bar {
-		return original
-	}
-	return "%{c}" + original
-}
-
-// Right aligns the original string to the right
-func Right(original string) string {
-	if mode == ModeI3Bar {
-		return original
-	}
-	return "%{r}" + original
-}
-
-// Dim apples a half-transparent white color to the original string
-func Dim(original string) string {
-	if mode == ModeI3Bar {
-		return original
-	}
-	return "%{F#C0" + secondaryColor + "}" + original + "%{F-}"
-}
-
-// Warning renders the original string orange
-func Warning(original string) string {
-	if mode == ModeI3Bar {
-		return original
-	}
-	return "%{F#FFAA00}" + original + "%{F-}"
-}
-
-// WarningBlink slowly blinks the original string oranage
-func WarningBlink(original string) string {
-	if mode == ModeI3Bar {
-		return original
-	}
-	if original == "" {
-		return ""
-	}
-
-	// convert unix nanoseconds to unix milliseconds
-	milliseconds := time.Now().UnixNano() / 1000000
-
-	// get alpha byte value
-	x := float32((milliseconds/2)%1000) / 1000
-	alpha := int(cubicEaseArc(x) * 255)
-
-	// limit alpha minimum to 50%
-	// we don't use math.Min because that would floop up the
-	// animation
-	alpha = alpha/2 + 255/2
-
-	hex := ByteToHex(alpha)
-
-	return "%{F#" + hex + "FFAA00}" + original + "%{F-}"
-}
-
-// FadeToDim colors the string according to interpolation from full white (0) to
-// dim (1)
-func FadeToDim(original string, interpolation float32) string {
-	if mode == ModeI3Bar {
-		return original
-	}
-	if original == "" {
-		return ""
-	}
-
-	// constrain
-	if interpolation < 0 {
-		interpolation = 0
-	} else if interpolation > 1 {
-		interpolation = 1
-	}
-
-	// quintic graph. apparently this is backwards
-	x := interpolation * -1
-	y := x*x*x*x*x + 1
-
-	color, err := interpolateColors("C0"+secondaryColor, "FF"+primaryColor, y)
-	if err != nil {
-		println(err.Error())
-		color = secondaryColor
-	}
-
-	return "%{F#" + color + "}" + original + "%{F-}"
-}
-
-// Alert blinks the original string red
-func Alert(original string) string {
-	if mode == ModeI3Bar {
-		return original
-	}
-	if original == "" {
-		return ""
-	}
-
-	// convert nanoseconds to milliseconds
-	milliseconds := time.Now().Nanosecond() / 1000000
-
-	// get alpha byte value
-	alpha := int(cubicEaseArc(float32(milliseconds)/1000) * 255)
-
-	// limit alpha minimum to 25%
-	alpha = alpha*3/4 + 255/4
-
-	hex := ByteToHex(alpha)
-
-	return "%{F#" + hex + "FF0000}" + original + "%{F-}"
 }
 
 // a cubic function except it's all concave down
@@ -186,30 +85,44 @@ func cubicEaseArc(x float32) float32 {
 	return cubic
 }
 
-func interpolateColors(first, second string, interpolation float32) (result string, err error) {
-	firstInt, err := strconv.ParseInt(first, 16, 64)
+func interpolateColors(first, second Color, interpolation float32) (result Color, err error) {
+	// rgbs
+
+	firstInt, err := strconv.ParseInt(first.RGBHex, 16, 64)
 	if err != nil {
 		return
 	}
 
-	secondInt, err := strconv.ParseInt(second, 16, 64)
+	secondInt, err := strconv.ParseInt(second.RGBHex, 16, 64)
 	if err != nil {
 		return
 	}
 
-	a1, a2 := (firstInt>>24)&0xFF, (secondInt>>24)&0xFF
 	r1, r2 := (firstInt>>16)&0xFF, (secondInt>>16)&0xFF
 	g1, g2 := (firstInt>>8)&0xFF, (secondInt>>8)&0xFF
 	b1, b2 := firstInt&0xFF, secondInt&0xFF
+
+	// alphas
+
+	a1, err := strconv.ParseInt(first.AlphaHex, 16, 64)
+	if err != nil {
+		return
+	}
+
+	a2, err := strconv.ParseInt(second.AlphaHex, 16, 64)
+	if err != nil {
+		return
+	}
 
 	a := int(float32(a1)*(1.0-interpolation) + float32(a2)*interpolation)
 	r := int(float32(r1)*(1.0-interpolation) + float32(r2)*interpolation)
 	g := int(float32(g1)*(1.0-interpolation) + float32(g2)*interpolation)
 	b := int(float32(b1)*(1.0-interpolation) + float32(b2)*interpolation)
 
-	resultInt := a<<24 + r<<16 + g<<8 + b
+	resultRGBInt := r<<16 + g<<8 + b
 
-	result = fmt.Sprintf("%08x", resultInt)
+	result.RGBHex = fmt.Sprintf("%08x", resultRGBInt)
+	result.AlphaHex = fmt.Sprintf("%08x", a)
 	return
 }
 
@@ -217,10 +130,13 @@ func interpolateColors(first, second string, interpolation float32) (result stri
 // (spaces in Lemonbar mode, comma + space in i3 mode)
 func ModuleSeparator() string {
 	switch mode {
-	case ModeI3Bar:
+	case I3JSONMode:
 		return ","
+	case LemonbarMode:
+		return "    "
+	default:
+		return "    "
 	}
-	return "    "
 }
 
 // Separator returns 4 spaces as a separator between data
@@ -228,51 +144,9 @@ func Separator() string {
 	return "    "
 }
 
-// ByteToHex takes a value from 0 to 255 and returns it in hexadecimal form
-func ByteToHex(value int) string {
-	// constrain to 0...255
-	if value > 255 {
-		return "FF"
-	} else if value < 0 {
-		return "00"
-	}
-	return strconv.FormatInt(int64(value), 16)
-}
-
-// SetSecondaryColor sets the secondary (dim) color of
-// muse-status.
-func SetSecondaryColor(color string) {
-	switch {
-	case len(color) == 8:
-		secondaryColor = color
-	case len(color) == 6:
-		secondaryColor = color + "ff"
-	default:
-		println("invalid secondary color. defaulting to gray")
-	}
-}
-
-// SetPrimaryColor sets the primary color of
-// muse-status.
-func SetPrimaryColor(color string) {
-	switch {
-	case len(color) == 8:
-		primaryColor = color
-	case len(color) == 6:
-		primaryColor = color + "ff"
-	default:
-		println("invalid primary color. defaulting to white")
-	}
-}
-
 // SetFormatMode sldksldksldkals;ala;skdkl;aslk
-func SetFormatMode(m string) {
-	switch m {
-	case "i3":
-		mode = ModeI3Bar
-	case "lemonbar":
-		mode = ModeLemonbar
-	}
+func SetFormatMode(m Mode) {
+	mode = m
 }
 
 // GetFormatMode returns the format mode.
